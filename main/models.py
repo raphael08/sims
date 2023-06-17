4fa11eaef6e1092903a8fdb74c194ccb2266e9f9
raphael08 <rsiphael@gmail.com>
2023-06-17 09:12:26 +0300

    v3

--- main/models.py
+++ main/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Sensordata(models.Model):
-    humidity_val = models.CharField(max_length=20)
-    temperature_val = models.CharField(max_length=30)
-    moisture_val = models.CharField(max_length=30)
-    waterLevel_val = models.CharField(max_length=30, default=0)
+    humidity_val = models.FloatField(default=0.00)
+    temperature_val = models.FloatField(default=0.00)
+    moisture_val = models.FloatField(default=0.00)
+    waterLevel_val = models.FloatField( default=0.00)
 
     def __str__(self):
             return f"Humidity: {self.humidity_val}, Temp: {self.temperature_val}, Moisture: {self.moisture_val}, waterLevel: {self.waterLevel_val}"
